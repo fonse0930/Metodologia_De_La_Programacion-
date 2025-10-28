5055e6415f35162df3e85d886431919748343f4b
D´Leon <2530020@.upv.edu.mx>
2025-10-28 10:42:26 -0600

    feat:understanding_strings

---
+++ src/undestanding_strings.py
@@ -0,0 +1,14 @@
+"""
+ 
+ String variable 
+ un string es de manera sencilla una serie de caracteres. En python, todo lo que se encuentre dentro de comillas simples (´ ´) o dobles (" ")
+ sera conciderado un string
+
+ Ejempo
+ "Esto es un string"
+ 'esto tambien es un string'
+ 'LE dije a un amigo "Python es mi lenguaje favorito" '
+ "El lenguaje 'Python' lleva el nombre por Mobty Python, no por la serpiente
+
+
+ """
